1d4530c13714e9a1d6a8618d4efe17ac3f6a23c0
himanshu yadav <hiyada@microsoft.com>
2019-07-11 14:23:23 +0530

    reverting

--- Gruntfile.js
+++ Gruntfile.js
@@ -76,7 +76,7 @@ module.exports = function(grunt) {
   }
 
   // Run task functions to configure Grunt.
-  //invokeConfigFn(taskConfigurations);
-  invokeConfigFn([registerDefinitions.build]);
+  invokeConfigFn(taskConfigurations);
+  invokeConfigFn(registerDefinitions);
 
 };
